bfea37cca2e91447b19a9d52d719d5bec588592c
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-02-22 20:21:03 -0600

    Update local.sh

--- services/scripts/local.sh
+++ services/scripts/local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-set -e
-
-[ -x /etc/rc.d/rc.local ] && /etc/rc.d/rc.local
+if [ "$1" = start ]; then
+  PATH=/usr/bin:/usr/sbin:/bin:/sbin
+  
+  [ -x /etc/rc.d/rc.local ] && /etc/rc.d/rc.local
+fi
