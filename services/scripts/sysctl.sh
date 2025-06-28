39623c33052793f60f414cdd87f24ddbefbbc727
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-06-28 17:32:48 -0500

    Update sysctl.sh

--- services/scripts/sysctl.sh
+++ services/scripts/sysctl.sh
@@ -4,8 +4,20 @@ export PATH=/usr/bin:/usr/sbin:/bin:/sbin
 set -e
 
 if [ "$1" != "stop" ]; then
-  if [ -x /sbin/sysctl ]; then
-    echo "Configuring kernel parameters: /sbin/sysctl -e --system"
-    /sbin/sysctl -e --system
-  fi
+# Configure kernel parameters:
+if [ -r /etc/default/sysctl ]; then
+  # Source user defined options:
+  . /etc/default/sysctl
+else
+  SYSCTL_OPTIONS="-e --system"
+fi
+if [ -x /sbin/sysctl -a -r /etc/sysctl.conf -a -z "$container" ]; then
+  echo "Configuring kernel parameters:  /sbin/sysctl $SYSCTL_OPTIONS"
+  /sbin/sysctl $SYSCTL_OPTIONS
+elif [ -x /sbin/sysctl -a -z "$container" ]; then
+  echo "Configuring kernel parameters:  /sbin/sysctl $SYSCTL_OPTIONS"
+  # Don't say "Applying /etc/sysctl.conf" or complain if the file doesn't exist
+  /sbin/sysctl $SYSCTL_OPTIONS 2> /dev/null | grep -v "Applying /etc/sysctl.conf"
+fi
+unset SYSCTL_OPTIONS
 fi;
