82df8819fd2547747b4a5e97d3e5eb84b875d455
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-07-31 19:10:46 -0500

    Update early-filesystems.sh

--- services/scripts/early-filesystems.sh
+++ services/scripts/early-filesystems.sh
@@ -124,32 +124,7 @@ fi
 if [ -z "$container" ]; then
   /sbin/swapon -a 2> /dev/null
 fi
-
-if ! grep -qw fuse /proc/filesystems; then
-   echo -n "Loading fuse module"
-   if ! modprobe fuse >/dev/null 2>&1; then
-   echo " failed!"
-  exit 1
-else
- echo "."
- fi
-else
-   echo "Fuse filesystem already available."
-fi
-
-if grep -qw fusectl /proc/filesystems && \
-  ! grep -qw $MOUNTPOINT /proc/mounts; then
-   echo -n "Mounting fuse control filesystem"
-  if ! mount -t fusectl fusectl $MOUNTPOINT >/dev/null 2>&1; then
-      echo " failed!"
-  exit 1
-else
-  echo "."
-fi
-else
-  echo "Fuse control filesystem already available."
-fi
-        
+       
 # Enable swapping on a ZRAM device:
 if [ -z "$container" -a -r /etc/default/zram ]; then
   . /etc/default/zram
